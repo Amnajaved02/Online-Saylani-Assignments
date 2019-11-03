2ade7883eeb56627c66e51a7d270604ff8b0c270
Amna Javed <amna.javed2000@gmail.com>
2019-11-03 22:26:16 +0500

    Python Assignment1

--- Assignment1.py
+++ Assignment1.py
@@ -1,41 +1,25 @@
-import timeit
-def BINSEARCH(List,value):
-    print(List)
-    for i in range (len(List)):
-        for j in range(i+1,len(List)):
-            if List[i]>List[j]:
-                List[i],List[j]=List[j],List[i]
-                unsort=True
-    if unsort ==True:
-        print("you entered an unordered List")
-    print("sorted List",List)
-    start=0
-    end=len(List)-1
-    mid=int((start+end)/2)
-    while start <=end and List[mid]!=value:
-        if value < List[mid]:
-            end=mid-1
-        else:
-            start=mid+1
-        mid=int((start+end)/2)
-    if List[mid]==value:
-        pos=mid
-        print("'",pos,"' is the index of the value=",value)
-    else:
-        pos=0
-    if pos ==0:
-        for q in List:
-            if q>=List[mid]:
-                List.insert(List.index(q),value)
-                print("updated List",List)
-                break
-L=[]
-x=int(input("declare no of elements in a list"))
-while x:
-    last=x
-    y=int(input("enter element in the list"))
-    L.append(y)
-    x-=1
-find=int(input("put a value you want to find in list"))
-BINSEARCH(L,find)
-print(timeit.timeit())
+import sys
+import datetime
+#Q2 
+print(sys.version)
+
+#Q3
+date=datetime.datetime.now()
+print(date)
+
+#Q4
+radius=int(input('Enter the radius of a circle'))
+area=(22/7)*(radius**2)
+print(area)
+
+#Q5
+First_name=input("Enter your first name")
+Last_name=input("Enter your last name")
+Full_name= Last_name+" "+First_name
+print(Full_name)
+
+#Q6
+x=input('Enter anything')
+y=input('Enter anything')
+z=x+y
+print(z)
